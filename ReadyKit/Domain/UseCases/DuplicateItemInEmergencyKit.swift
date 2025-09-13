fc8804dd42f3d527de05d35d09a2ebf5d6f25475
Luis Wu <15936536+luiswdy@users.noreply.github.com>
2025-09-13 16:06:24 +0800

    fix: unnecessary return

--- ReadyKit/Domain/UseCases/DuplicateItemInEmergencyKit.swift
+++ ReadyKit/Domain/UseCases/DuplicateItemInEmergencyKit.swift
@@ -2,10 +2,12 @@ import Foundation
 
 struct DuplicateItemInEmergencyKitUseCase {
     private let itemRepository: ItemRepository
+
     init(itemRepository: ItemRepository) {
         self.itemRepository = itemRepository
     }
+
     func execute(item: Item, emergencyKit: EmergencyKit) throws {
-        return try itemRepository.duplicate(item: item, to: emergencyKit)
+        try itemRepository.duplicate(item: item, to: emergencyKit)
     }
 }
